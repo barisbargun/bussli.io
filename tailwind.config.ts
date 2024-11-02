76de0c1e470710570ab75a29fb2d33306038bca3
Barış Olgun <barsbargun@gmail.com>
2024-11-02 23:25:09 +0300

    chore: remove unused packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -41,7 +41,7 @@ const config = {
       }
     }
   },
-  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')]
+  plugins: [require('@tailwindcss/typography')]
 } satisfies Config
 
 export default config
