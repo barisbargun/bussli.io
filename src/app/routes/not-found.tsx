fc802615aa754731f4dacf6f46e6e21606f896cb
Barış Olgun <barsbargun@gmail.com>
2024-11-02 23:04:27 +0300

    feat: update not-found page

--- src/app/routes/not-found.tsx
+++ src/app/routes/not-found.tsx
@@ -1,13 +1,10 @@
-import { Link } from 'react-router-dom'
+import { PageSection } from '@/components/global/page-section'
+import { assets } from '@/config/assets'
 
 export const NotFoundRoute = () => {
   return (
-    <div className="mt-52 flex flex-col items-center font-semibold">
-      <h1>404 - Not Found</h1>
-      <p>Sorry, the page you are looking for does not exist.</p>
-      <Link replace to="/">
-        Go to Home
-      </Link>
-    </div>
+    <PageSection nestedClassName="min-h-screen items-center justify-center">
+      <img alt="not-found" className="w-full max-w-4xl" src={assets.svg.illustrations.notFound} />
+    </PageSection>
   )
 }
