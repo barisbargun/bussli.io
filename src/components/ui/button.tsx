f057d2306214922c5836fca06816ad5559edd279
Barış Olgun <53085074+barisbargun@users.noreply.github.com>
2024-11-13 19:15:34 +0300

    feat(button): add new variants

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,12 +19,15 @@ const buttonVariants = cva(
           'bg-slate-100 text-slate-900 shadow-sm hover:bg-slate-100/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80',
         ghost:
           'hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50',
-        link: 'text-slate-900 underline-offset-4 hover:underline dark:text-slate-50'
+        link: 'text-slate-900 underline-offset-4 hover:underline dark:text-slate-50',
+        indigo:'border-indigo-500 bg-indigo-600 hover:bg-indigo-700 active:bg-indigo-800 font-bold text-white',
+        indigoTransparent:'hover:bg-indigo-600/15 active:bg-indigo-600/20 tracking-tight bg-indigo-600/10 justify-between'
       },
       size: {
         default: 'h-9 px-4 py-2',
         sm: 'h-8 rounded-md px-3 text-xs',
         lg: 'h-10 rounded-md px-8',
+        xl:'h-12 rounded-lg px-10 text-lg',
         icon: 'size-9'
       }
     },
